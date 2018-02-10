8bcaccd3fc7e1a51928767c2a060c8e3e5111679
Anwesh43 <anweshthecool0@gmail.com>
2018-02-10 13:16:24 +0530

    adding VshapedState

--- VshapedComponent.js
+++ VshapedComponent.js
@@ -50,9 +50,39 @@ class VShaped {
         context.restore()
     }
     update(stopcb) {
-      
+
     }
     startUpdating(startcb) {
 
+    }
+}
+class VShapedState {
+    constructor() {
+        this.scales = [0,0,0,0,0,0]
+        this.dir = 0
+        this.prevScale = 0
+        this.j = 0
+        this.jDir = 1
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1*this.dir
+        if(Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scales[this.j]
+            this.j += this.jDir
+            if(this.j == this.scales.length || this.j  == -1) {
+                this.jDir *= -1
+                this.j += this.jDir
+                this.prevScale = this.scales[this.j]
+            }
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1 - 2*this.prevScale
+            startcb()
+        }
     }
 }
